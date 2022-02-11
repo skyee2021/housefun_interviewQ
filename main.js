570c0352636f10838322e6496817dc5d5d786149
skyee20211@gmail.com <skyee20211@gmail.com>
2022-02-11 10:47:00 +0800

    add autocomplete

--- main.js
+++ main.js
@@ -9,9 +9,37 @@ document.addEventListener('DOMContentLoaded', async() => {
   // console.log(request);
   // console.log(request[0]);
   // let aa=request[0]
-  // console.log(aa.districts[0]);
+  // console.losg(aa.districts[0]);
   const newD = document.createElement('div');
   newD.innerHTML = "<div >城市 </div>";
+  const cityArea = [];
+  const areaList = Object.keys(request).map(function(e){
+    let city = request[e].name;
+    // console.log(request[e].districts);
+    // let areas = request[e].districts.forEach(element=>{
+      // console.log(city);
+    //   return element.name;
+    // });
+    let areas = request[e].districts.forEach(element=>{
+      // console.log(request[e].name);
+      // console.log(element.name);
+      let ans = request[e].name+element.name;
+      // console.log(ans);
+      cityArea.push(ans);
+      // return request[e].name+element.name
+    });
+    // console.log(cityArea);
+
+    // for(let j=0; j<request[e].districts.length; j++) {
+    //   let cityname = request[e].name;
+    //   let areaName = request[e].districts[j].name;
+    //   console.log(areaName);
+    //   return request[e].name + request[e].districts[j].name
+    // };
+    // console.log(area);
+    return cityArea;
+  })
+  // console.log(cityArea);
 
   
   //把所有資料render出來
@@ -45,5 +73,11 @@ document.addEventListener('DOMContentLoaded', async() => {
   });
 
 
+  //自動填寫
+  //Awesomplete
+  new Awesomplete(document.getElementById('search'), {
+    list: cityArea
+  });
+  
 
 });
